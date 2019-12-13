07a5295130c71092eccf81e3d31a8efaca69bab5
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-13 08:25:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,5 +3,16 @@ def countdown(int)
   puts "#{int} SECOND(S)!"
   int -= 1
 end
-return "HAPPY NEW YEAR"
+return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep(1)
+end
+"HAPPY NEW YEAR!"
+end
+
